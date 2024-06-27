934e1da1f9b24b9b519c05abeb2c5658ee2ffaa2
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2024-06-28 01:35:23 +0700

    modify

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 import React, { useState } from 'react';
 import data from '../../assets/memberdropdown.json';
-import ChangeInvestmentType from '../components/ChangeInvestmentType/ChangeInvestmentType';
 
 export default function Dashboard() {
     const waitingApproval = 1; // Define the waitingApproval variable
-
-    const [isChangeInvestmentTypeOpen, setIsChangeInvestmentTypeOpen] = useState(false);
-
-    const openChangeInvestmentType = () => setIsChangeInvestmentTypeOpen(true);
-    const closeChangeInvestmentType = () => setIsChangeInvestmentTypeOpen(false);
     
     return (
         <>
@@ -37,19 +31,6 @@ export default function Dashboard() {
             <div className="table_body">
                 <button>등록</button>
             </div>
-            <div>
-        <button
-          onClick={openChangeInvestmentType}
-        >
-          Open ChangeInvestmentType
-        </button>
-        <ChangeInvestmentType
-          isOpen={isChangeInvestmentTypeOpen}
-          onClose={closeChangeInvestmentType}
-          imageUrl="/warning.svg"
-          modalContent="선택된 2건의 승인상태를 변경하시겠습니까?"
-        />
-      </div>
         </>
     );
 }
